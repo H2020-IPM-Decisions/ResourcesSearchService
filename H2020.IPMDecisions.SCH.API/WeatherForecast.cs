a740254b949cf2633eaf1dbd8d69eb96f1edecaa
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2022-04-06 13:23:23 +0200

    Move version to .Net 5

--- H2020.IPMDecisions.SCH.API/WeatherForecast.cs
+++ H2020.IPMDecisions.SCH.API/WeatherForecast.cs
@@ -1,12 +1,15 @@
-namespace H2020.IPMDecisions.SCH.API;
+using System;
 
-public class WeatherForecast
+namespace H2020.IPMDecisions.SCH.API
 {
-    public DateTime Date { get; set; }
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
 
-    public int TemperatureC { get; set; }
+        public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
-    public string? Summary { get; set; }
+        public string Summary { get; set; }
+    }
 }
