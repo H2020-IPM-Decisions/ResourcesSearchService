3ce30b8e141624447fb79df6c6f2e42f5ce5a9a3
Antonio Calatayud <antonio.calatayud@adas.co.uk>
2022-04-06 17:54:46 +0200

    Add pest filter to models

--- H2020.IPMDecisions.SCH.API/Controllers/SearchController.cs
+++ H2020.IPMDecisions.SCH.API/Controllers/SearchController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Mime;
 using System.Threading.Tasks;
 
@@ -41,7 +42,23 @@ namespace H2020.IPMDecisions.SCH.API.Controllers
             {
                 var listOfDss = await this.microservicesCommunication.GetAllListOfDssFromDssMicroservice(searchRequestDto.SpecificCrop, searchRequestDto.Language);
                 if (listOfDss == null) throw new SystemException("System not available, please try again later.");
-                return Ok(listOfDss);
+
+                var listOfModels = listOfDss
+                    .SelectMany(d => d.DssModelInformation);
+                if (!string.IsNullOrEmpty(searchRequestDto.PestType))
+                {
+                    listOfModels = listOfModels
+                        .Where(m => m.Pests
+                            .Contains(searchRequestDto.PestType.ToUpper()));
+                }
+                // if (!string.IsNullOrEmpty(searchRequestDto.Country)){}
+                // if (!string.IsNullOrEmpty(searchRequestDto.Sector)){}
+                // if (!string.IsNullOrEmpty(searchRequestDto.ResourceType)){}
+                listOfDss = listOfDss
+                    .ToList();
+                // ToDo: map models to DSS to responseDto
+
+                return Ok(listOfModels);
             }
             catch (Exception ex)
             {
